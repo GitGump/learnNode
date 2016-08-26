5ba6323bb359dbd8441ee82b38b12a5d2f25c957
Tian Xudong <tianxudong@tp-link.net>
2016-08-26 15:49:37 +0800

    Add lesson 3 4 5 6

---
+++ lesson5/test/index.test.js
@@ -0,0 +1,30 @@
+var index = require('../index');
+var should = require('should');
+
+describe('test/index.test.js', function() {
+  it('should equal 55 when n === 10', function() {
+    index.fibonacci(10).should.equal(55);
+  });
+
+  it('should equal 1 when n === 1', function() {
+    index.fibonacci(1).should.equal(1);
+  });
+
+  it('should throw when n > 10', function() {
+    (function() {
+      index.fibonacci(11);
+    }).should.throw('n should <= 10');
+  });
+
+  it('should throw when n < 0', function() {
+    (function() {
+      index.fibonacci(-1);
+    }).should.throw('n should >= 0');
+  });
+
+  it('should throw when n isnt Number', function() {
+    (function() {
+      index.fibonacci('Hehe');
+    }).should.throw('n should be a Number');
+  });
+});
